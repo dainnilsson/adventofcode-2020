a0df7f758f2d6fb327aec4a3a54fdea8b831a3da
Dain Nilsson <dain@yubico.com>
2020-12-02 09:13:38 +0100

    Fix tests.

--- tests/test_solutions.py
+++ tests/test_solutions.py
@@ -3,31 +3,47 @@ import pytest
 import importlib
 
 
-@pytest.mark.parametrize(
-    'day, part',
-    [(day, part) for day in range(1, 26) for part in ("a", "b")]
-)
-def test_solution(day, part):
+@pytest.fixture(scope="session", params=list(range(1, 26)))
+def day(request):
+    return request.param
+
+
+@pytest.fixture(scope="session", params=["a", "b"])
+def part(request):
+    return request.param
+
+
+@pytest.fixture(scope="session")
+def expected(day, part):
+    out_f = "output/%d%s.txt" % (day, part)
+    if not os.path.isfile(out_f):
+        return None
+
+    with open(out_f) as f:
+        return f.read()
+
+
+@pytest.fixture(scope="session")
+def solution(day):
     in_f = "input/%d.txt" % day
     if not os.path.isfile(in_f):
         pytest.skip("No input")
 
-    out_f = "output/%d%s.txt" % (day, part)
-    if not os.path.isfile(out_f):
-        pytest.skip("No output")
-
     with open(in_f) as f:
         data = f.read()
 
-    with open(out_f) as f:
-        out = f.read()
-
     module = importlib.import_module("solutions.day%d" % day)
     solver = module.solve(data, lambda *_: None)
+    return next(solver), next(solver)
 
-    a = next(solver)
-    if part == "a":
-        assert "%s\n" % a == out
-    else:
-        b = next(solver)
-        assert "%s\n" % b == out
+
+@pytest.fixture(scope="session")
+def output(solution, part):
+    return solution[0 if part == "a" else 1]
+
+
+def test_solution(expected, output):
+    if expected is None:
+        pytest.skip("No output")
+
+    assert "%s\n" % output == expected
